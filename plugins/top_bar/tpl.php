ca7ee6ca1d2125e8504f08e98961c5b1af2c1c5d
Tomas Doubek <thomas.doubek@gmail.com>
2020-12-20 14:11:08 +0100

    remove comment code

--- plugins/top_bar/tpl.php
+++ plugins/top_bar/tpl.php
@@ -14,28 +14,11 @@
                 <div class="jazyky">
                     <ul>
                         <li><a href="<?php echo WWW_PATH; ?>"><i class="fa fa-2x fa-home"></i></a></li>
-                        <?php
-                        /*$lngArr = MultyLanguage::activeVendorLangs();
-                        if (count($lngArr) > 1) {
-                            foreach (MultyLanguage::activeVendorLangs() as $lg) {
-                                $urlLg = WWW_PATH . "" . $lg . "/" . $rest->webhook(1);
-                                ?>
-                                <li>
-                                    <a href="<?php echo $urlLg; ?>" >
-                                        <img src="<?php echo WWW_PATH . "dnt-view/layouts/wp_tpl_2/images/flags/flag_" . $lg . ".png"; ?>" alt="<?php echo $lg; ?>"></a>
-                                </li>
-                                <?php
-                            }
-                        }*/
-                        ?>
                     </ul>
                 </div>
             </div>
             <div class="col-sm-4 col-xs-4 clearfix">
                 <i class="fa fa-search search-btn pull-right"></i> 
-                <?php /* <ul class="topbar-list topbar-log_reg pull-right visible-sm-block visible-md-block visible-lg-block">
-                  <li class="cd-log_reg home"><a class="cd-signin" href="javascript:void(0);"><?php echo $translate['prihlasit'];?></a></li>
-                  </ul> */ ?>
             </div>
         </div>
         <!--/end row-->
